821c9ff5916691325d5c5abb0ad8a894144002fc
Debaditya Sen <debaditya_sen@srmuniv.edu.in>
2020-07-22 05:24:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import "firebase/firestore";
 import "firebase/analytics";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBBdmHjgvXljKEbP_KAF_tEK1D0MF1jslc",
-  authDomain: "petter-site.firebaseapp.com",
-  databaseURL: "https://petter-site.firebaseio.com",
-  projectId: "petter-site",
-  storageBucket: "petter-site.appspot.com",
-  messagingSenderId: "440116767549",
-  appId: "1:440116767549:web:ef9fabb9ee58d8191cf354",
-  measurementId: "G-SJ0G40R4F0"
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
